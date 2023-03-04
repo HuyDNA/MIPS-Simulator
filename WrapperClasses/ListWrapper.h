6a0fcd03ec9f4d137c4e2ea004ba8f7709baecaa
phamvannhatvu <phamvannhatvu@gmail.com>
2023-03-05 01:01:02 +0700

    CREATE FOLDER WRAPPERCLASSES AND PUT LISTWRAPPER INTO IT

---
+++ WrapperClasses/ListWrapper.h
@@ -0,0 +1,83 @@
+#ifndef LIST_WRAPPER_MIPS_SIMULATOR
+#define LIST_WRAPPER_MIPS_SIMULATOR
+
+#include <vector>
+
+using std::vector;
+
+template<class T>
+Class ListWrapper {
+private:
+    vector<T> list;
+public:
+    void pushBack(const T& item) {
+        list.push_back(item);
+    }
+
+    void pushFront(const T& item) {
+        list.push_back(item);
+        for (int i = list.size() - 1; i > 0; --i)
+        {
+            list[i] = list[i - 1];
+        }
+        list[0] = item;
+    }
+
+    void popBack() {
+        list.pop_back();
+    }
+
+    void popFront() {
+        for (int i = 0; i < list.size() - 1; ++i)
+        {
+            list[i] = list[i + 1];
+        }
+        list.pop_back();
+    }
+
+    int getSize() const {
+        return list.size();
+    }
+
+    bool empty() const {
+        return list.empty();
+    }
+
+    ListWrapper() : list(vector<T>{}) {
+        
+    }
+
+    ListWrapper<T>(const ListWrapper<T> &wrapper) : list(vector<T>(wrapper.list)) {
+
+    }
+
+    ListWrapper<T>(ListWrapper<T> &&wrapper) noexcept : list(wrapper.list) {
+        
+    }
+
+    auto cbegin() const {
+        return list.cbegin();
+    }
+
+    auto cend() const {
+        return list.cend();
+    }
+
+    auto begin() {
+        return list.begin();
+    }
+
+    auto end() {
+        return list.end();
+    }
+
+    const T& operator[] (int id) const {
+        return list.at(id);
+    }
+
+    T& operator[] (int id) {
+        return list.at(id);
+    }
+};
+
+#endif
