fcd575ee05000169d066b8561d7bdab2a05fd61d
HuyDNA <huydo862003@gmail.com>
2023-03-07 22:28:03 +0700

    CHANGE ListWrapper -> ListWrapper<T>
    Injected class name does not work here, it needs <T> to make the call depenendent

--- WrapperClasses/BoundedListWrapper.h
+++ WrapperClasses/BoundedListWrapper.h
@@ -10,39 +10,39 @@
 using std::vector;
 
 template<class T>
-Class BoundedListWrapper : public ListWrapper<T>{
+Class BoundedListWrapper : public ListWrapper<T> {
 private:
     const int capacity;
 public:
-    BoundedListWrapper(int capacity = 0) : ListWrapper(), capacity(capacity) {
+    BoundedListWrapper(int capacity = 0) : ListWrapper<T>(), capacity(capacity) {
         
     }
 
-    BoundedListWrapper(const BoundedListWrapper &wrapper) : ListWrapper(wrapper), capacity(wrapper.capacity) {
+    BoundedListWrapper(const BoundedListWrapper &wrapper) : ListWrapper<T>(wrapper), capacity(wrapper.capacity) {
 
     }
 
-    BoundedListWrapper(BoundedListWrapper &&wrapper) noexcept : ListWrapper(std::move(wrapper)), capacity(wrapper.capacity) {
+    BoundedListWrapper(BoundedListWrapper &&wrapper) noexcept : ListWrapper<T>(std::move(wrapper)), capacity(wrapper.capacity) {
         
     }
 
     void pushBack(const T &item) override {
         if (this->getSize() == capacity)
             throw std::length_error("Size of bounded list exceeds capacity");
-        ListWrapper::pushBack(item);
+        ListWrapper<T>::pushBack(item);
     }
 
     void pushFront(const T &item) override {
         if (this->getSize() == capacity)
             throw std::length_error("Size of bounded list exceeds capacity");
-        ListWrapper::pushFront(item);
+        ListWrapper<T>::pushFront(item);
     }
 
     virtual T& operator[] (int id) override {
         while (this->getSize() <= id) {
             this->pushBack(0);
         }
-        return ListWrapper::operator[](id);
+        return ListWrapper<T>::operator[](id);
     }
 };
 
